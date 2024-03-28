e4bd74293f6d3144112b2310d46926def78c2f1c
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-03-28 15:41:56 +0100

    Add py files already created and working

---
+++ src/model/newspaper.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from flask_restx import Model
+
+from .issue import Issue
+
+
+class Newspaper(object):
+
+    def __init__(self, paper_id: int, name: str, frequency: int, price: float):
+        self.paper_id: int = paper_id
+        self.name: str = name
+        self.frequency: int = frequency  # the issue frequency (in days)
+        self.price: float = price  # the monthly price
+        # This is expected to take a list of Issues as an argument.
+        # Each element is an instance of the 'Issue' class.
+        self.issues: List[Issue] = []
