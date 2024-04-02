9d02b9be6b5dc6cd9f8f944a378cbea5f5907832
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-04-02 23:45:34 +0200

    Redefining the constructor

--- src/model/issue.py
+++ src/model/issue.py
@@ -1,9 +1,13 @@
 
 class Issue(object):
 
-    def __init__(self, releasedate, released: bool = False):
-        self.releasedate = releasedate
+    # I considered adding an ID because it is easy to be used for methods such as remove or update
+    def __init__(self, issue_id: int, release_date, number_of_pages: int, released: bool = False):
+        self.issue_id = issue_id
+        self.release_date = release_date
+        self.number_of_pages = number_of_pages
         self.released: bool = released
+        self.editor = None
 
     def set_editor(self, editor):
         # TODO: Implement me!
