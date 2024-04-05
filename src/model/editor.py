6a0b2c40e8a839882cd954e197c368d68e68a056
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-04-06 01:07:24 +0200

    Change the method when removing an editor

--- src/model/editor.py
+++ src/model/editor.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from flask_restx import Model
 
@@ -11,13 +11,12 @@ class Editor(object):
         self.editor_id = editor_id
         self.editor_name = editor_name
         self.address = address
-        self.newspaper: List[Newspaper] = []
-        # Create a dictionary with newspapers and issues
-        # self.newspaper_issues: Dict[Newspaper, Issue] =
+        # Use a list of newspaper IDs instead of instances
+        self.newspaper_ids: List[int] = []
 
 
 # Create another class to handle the editors and their methods
-class System:
+class SystemEditor:
     def __init__(self):
         # Store instances of 'Editor' in the system
         self.editors: List[Editor] = []
@@ -28,30 +27,58 @@ class System:
 
     # Remove editor and transfer all issues to another editor of the same newspaper
     def remove_editor(self, editor_id: int):
-        editor_to_remove = None
+        # Find the editor we want to remove based on his ID
+        editor_to_remove = self.find_editor(editor_id)
+        if not editor_to_remove:
+            return None  # NOT FOUND
+
+        # Transfer issues
+        for newspaper_id in editor_to_remove.newspaper_ids:
+            # Find a possible editor with the same newspaper
+            new_editor = self.find_another_editor(newspaper_id, editor_id)
+            if new_editor:
+                self.transfer_issues(newspaper_id, editor_to_remove, new_editor)
+
+    def find_editor(self, editor_id: int) -> Optional[Editor]:
         for editor in self.editors:
             if editor.editor_id == editor_id:
-                # Find the editor based on ID
-                editor_to_remove = editor
-                break
-
-        # If the editor is found
-        if editor_to_remove:
-            # Try to find another editor with the same newspapers
-            for newspaper in editor_to_remove.newspaper:
-                new_editor = self.find_editor(newspaper.paper_id, editor_id)
-                if new_editor:
-                    # Transfer issues
-                    for issue in newspaper.issues:
-                        issue.editor_id = new_editor.editor_id
-
-            # Remove the editor
-            self.editors.remove(editor_to_remove)
-
-    def find_editor(self, newspaper_id: int, remove_editor_id: int) -> Editor:
-        for editor in self.editors:
-            if newspaper_id in editor.newspaper and editor.editor_id != remove_editor_id:
                 return editor
+        return None
+
+    def find_another_editor(self, newspaper_id: int, remove_editor_id: int) -> Optional[Editor]:
+        for editor in self.editors:
+            if newspaper_id in editor.newspaper_ids and editor.editor_id != remove_editor_id:
+                return editor
+        return None
+
+    def transfer_issues(self, newspaper_id: int, from_editor: Editor, to_editor: Editor):
+        pass
+
+    # def remove_editor(self, editor_id: int):
+    #     editor_to_remove = None
+    #     for editor in self.editors:
+    #         if editor.editor_id == editor_id:
+    #             # Find the editor based on ID
+    #             editor_to_remove = editor
+    #             break
+    #
+    #     # If the editor is found
+    #     if editor_to_remove:
+    #         # Try to find another editor with the same newspapers
+    #         for newspaper in editor_to_remove.newspaper:
+    #             new_editor = self.find_editor(newspaper.paper_id, editor_id)
+    #             if new_editor:
+    #                 # Transfer issues
+    #                 for issue in newspaper.issues:
+    #                     issue.editor_id = new_editor.editor_id
+    #
+    #         # Remove the editor
+    #         self.editors.remove(editor_to_remove)
+    #
+    # def find_editor(self, newspaper_id: int, remove_editor_id: int) -> Editor:
+    #     for editor in self.editors:
+    #         if newspaper_id in editor.newspaper and editor.editor_id != remove_editor_id:
+    #             return editor
 
     def update_editor(self, editor_id: int, name: str = None, address: str = None, newspaper: List[Newspaper] = None):
         for editor in self.editors:
