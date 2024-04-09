e07b243fcae9175cc82e423bca66e90b7f93d29f
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-04-09 21:02:22 +0200

    TODO: the list of subscriptions

--- src/model/subscriber.py
+++ src/model/subscriber.py
@@ -8,21 +8,21 @@ class Subscriber:
         self.id = subscriber_id
         self.name = name
         self.address = address
-        # The list of newspapers that they are subscribed to
+        # TODO: The list of newspapers that they are subscribed to
         self.subscriptions: List[Newspaper] = []
 
-    def subscribe(self, newspaper: Newspaper):
-        self.subscriptions.append(newspaper)
-
-    def unsubscribe(self, newspaper: Newspaper):
-        self.subscriptions = [sub for sub in self.subscriptions if sub.paper_id != newspaper.paper_id]
-
-    def add(self):
-        pass
-
-    def remove(self):
-        pass
-
-    def update(self):
-        pass
+    # def subscribe(self, newspaper: Newspaper):
+    #     self.subscriptions.append(newspaper)
+    #
+    # def unsubscribe(self, newspaper: Newspaper):
+    #     self.subscriptions = [sub for sub in self.subscriptions if sub.paper_id != newspaper.paper_id]
+    #
+    # def add(self):
+    #     pass
+    #
+    # def remove(self):
+    #     pass
+    #
+    # def update(self):
+    #     pass
 
