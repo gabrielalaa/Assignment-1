1e477790ca4535e9885fbd6864ceca58a3cd729c
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-04-10 22:57:29 +0200

    Considering changing the type of release date

--- src/model/issue.py
+++ src/model/issue.py
@@ -4,6 +4,7 @@ class Issue(object):
     # I considered adding an ID because it is easy to be used for methods such as remove or update
     def __init__(self, issue_id: int, release_date, number_of_pages: int, released: bool = False):
         self.issue_id = issue_id
+        # self.release_date = datetime.fromisoformat(release_date) if isinstance(release_date, str) else release_date
         self.release_date = release_date
         self.number_of_pages = number_of_pages
         self.released: bool = released
