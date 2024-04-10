0f8177d025aa0a115d2acaeb4f4ef48fe2dfbbc6
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-04-10 17:48:27 +0200

    Creating the methods for issues; Creating a structure for the future methods

--- src/model/agency.py
+++ src/model/agency.py
@@ -2,6 +2,8 @@ from typing import List, Union, Optional
 
 from .issue import Issue
 from .newspaper import Newspaper
+from .subscriber import Subscriber
+from .editor import Editor
 
 
 class Agency(object):
@@ -9,6 +11,8 @@ class Agency(object):
 
     def __init__(self):
         self.newspapers: List[Newspaper] = []
+        self.subscribers: List[Subscriber] = []
+        self.editors: List[Editor] = []
 
     # This ensures that only one instance of 'Agency' exists (Singleton pattern)
     @staticmethod
@@ -37,36 +41,82 @@ class Agency(object):
         self.newspapers.remove(paper)
 
     # METHODS for issues
+    def get_issue(self, paper_id: int, issue_id: int) -> Optional[Issue]:
+        newspaper = self.get_newspaper(paper_id)
+        if newspaper is not None:
+            for issue in newspaper.issues:
+                if issue.issue_id == issue_id:
+                    return issue
+            return None
+        else:
+            raise ValueError(f"A newspaper with ID {paper_id} doesn't exist!")
+
     # TODO: check if paper_id: int and issue; error handling
-    def add_issue_to_newspaper(self, paper_id: Union[int, str], issue: Issue):
+    def add_issue_to_newspaper(self, paper_id: int, issue: Issue):
         newspaper = self.get_newspaper(paper_id)
         if newspaper is not None:
             newspaper.issues.append(issue)
         else:
-            # What if the newspaper does not exist?
-            pass
+            raise ValueError(f"A newspaper with ID {paper_id} doesn't exist!")
 
-    # example with error handling
-    # def add_issue_to_newspaper(self, paper_id: Union[int, str], issue: Issue):
-    #     newspaper = self.get_newspaper(paper_id)
-    #     if newspaper is not None:
-    #         if not any(i.issue_id == issue.issue_id for i in newspaper.issues):
-    #             newspaper.issues.append(issue)
-    #         else:
-    #             # Handle the case where the issue ID already exists
-    #             raise ValueError(f"Issue with ID {issue.issue_id} already exists in newspaper {paper_id}.")
-    #     else:
-    #         # Handle the case where the newspaper does not exist
-    #         raise ValueError(f"Newspaper with ID {paper_id} does not exist.")
-
-    # TODO: check if paper_id: int and issue; error handling
-    def remove_issue_from_newspaper(self, paper_id: Union[int, str], issue_id: int):
+    def release_issue(self, paper_id: int, issue_id: int):
         newspaper = self.get_newspaper(paper_id)
         if newspaper is not None:
-            newspaper.issues = [issue for issue in newspaper.issues if issue.issue_id != issue_id]
-        else:
-            pass
+            issue = self.get_issue(paper_id, issue_id)
+            if issue is not None:
+                issue.released = True
+            else:
+                raise ValueError(f"A newspaper with ID {paper_id} doesn't exist!")
 
-    # TODO: implement me
-    def update_issue_in_newspaper(self, paper_id: Union[int, str], issue_id: int):
+    def specify_editor(self):
         pass
+
+
+    # # TODO: check if paper_id: int and issue; error handling
+    # def remove_issue_from_newspaper(self, paper_id: Union[int, str], issue_id: int):
+    #     newspaper = self.get_newspaper(paper_id)
+    #     if newspaper is not None:
+    #         newspaper.issues = [issue for issue in newspaper.issues if issue.issue_id != issue_id]
+    #     else:
+    #         pass
+    #
+    # # TODO: implement me
+    # def update_issue_in_newspaper(self, paper_id: Union[int, str], issue_id: int):
+    #     pass
+
+    #    def add_editor(self, new_editor: Editor):
+    #         # TODO: assert that ID does not exist  yet (or create a new one)
+    #         self.editors.append(new_editor)
+    #
+    #     def get_editor(self, editor_id: Union[int, str]) -> Optional[Editor]:
+    #         for editor in self.editors:
+    #             if editor.editor_id == editor_id:
+    #                 return editor
+    #         return None
+    #
+    #     def all_editor(self) -> List[Editor]:
+    #         return self.editors
+    #
+    #     def remove_editor(self, editor: Editor):
+    #         self.editors.remove(editor)
+    #
+    #     def editor_issues(self , editor_id: Union[int, str]) -> list[Issue] | None:
+    #         for editor in self.editors:
+    #             if editor.editor_id == editor_id:
+    #                 return editor.issues
+    #         return None
+    #     def add_subscriber(self, new_subscriber: Subscriber):
+    #         # TODO: assert that ID does not exist  yet (or create a new one)
+    #         self.subscribers.append(new_subscriber)
+    #
+    #     def get_subscriber(self, subscriber_id: Union[int, str]) -> Optional[Subscriber]:
+    #         for subscriber in self.subscribers:
+    #             if subscriber.subscriber_id == subscriber_id:
+    #                 return subscriber
+    #         return None
+    #
+    #     def all_subscriber(self) -> List[Subscriber]:
+    #         return self.subscribers
+    #
+    #     def remove_subscriber(self, subscriber: Subscriber):
+    #         self.subscribers.remove(subscriber)
