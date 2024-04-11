2f0ec59f87b185d66ef29c5e95c9d8651cae8906
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-04-12 01:40:23 +0200

    Change the constructor based on the new issue_model which contains editor_id

--- src/model/issue.py
+++ src/model/issue.py
@@ -1,11 +1,11 @@
 class Issue(object):
-    def __init__(self, issue_id: int, release_date: str, number_of_pages: int, released: bool = False):
+    def __init__(self, issue_id: int, release_date: str, number_of_pages: int, released: bool = False, editor_id: int = None):
         self.issue_id = issue_id
         self.release_date = release_date
         self.number_of_pages = number_of_pages
         self.released: bool = released
-        self.editor = None
+        self.editor_id = editor_id
 
-    def set_editor(self, editor):
-        self.editor = editor
+    def set_editor(self, editor_id: int):
+        self.editor_id = editor_id
 
