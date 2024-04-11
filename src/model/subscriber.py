f81e4049c12fe0c5652ac5febdde052f0cb3a7d4
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-04-12 01:56:33 +0200

    Give up the methods initially thought

--- src/model/subscriber.py
+++ src/model/subscriber.py
@@ -11,18 +11,3 @@ class Subscriber:
         # TODO: The list of newspapers that they are subscribed to
         self.subscriptions: List[Newspaper] = []
 
-    # def subscribe(self, newspaper: Newspaper):
-    #     self.subscriptions.append(newspaper)
-    #
-    # def unsubscribe(self, newspaper: Newspaper):
-    #     self.subscriptions = [sub for sub in self.subscriptions if sub.paper_id != newspaper.paper_id]
-    #
-    # def add(self):
-    #     pass
-    #
-    # def remove(self):
-    #     pass
-    #
-    # def update(self):
-    #     pass
-
