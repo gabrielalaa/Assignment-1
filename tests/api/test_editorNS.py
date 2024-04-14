91ca1f5648b703cdeda71bec60b24df89dddf9f5
Rădulescu Carla Gabriela <carlagabrielaradulescu@gmail.com>
2024-04-14 21:24:34 +0200

    Done with testing editorNS

--- tests/api/test_editorNS.py
+++ tests/api/test_editorNS.py
@@ -139,3 +139,68 @@ def test_delete_editor(client, agency):
     # Try to delete the same editor
     delete_response = client.delete(f"/newspaper/{editor_response['editor_id']}")
     assert "was not found" in delete_response.get_data(as_text=True)
+
+
+def test_assign_newspaper_to_editor(client, agency):
+    # Add the editor
+    new_editor = client.post('/editor/',
+                             json={
+                                 "editor_id": 10000,
+                                 "editor_name": "Ana",
+                                 "address": "San Francisco"
+                             })
+    parsed = new_editor.get_json()
+    editor_response = parsed["editor"]
+    editor_id = editor_response["editor_id"]
+
+    # Add the newspaper
+    new_paper_response = client.post("/newspaper/",
+                                     json={
+                                         "name": "Simpsons Comic",
+                                         "frequency": 7,
+                                         "price": 3.14
+                                     })
+
+    parsed = new_paper_response.get_json()
+    paper_response = parsed["newspaper"]
+
+    # Extract the newspaper ID
+    paper_id = paper_response["paper_id"]
+
+    # Assign
+    response = client.post(f'/editor/{editor_id}/newspapers',
+                           json={"paper_id": paper_id})
+
+    # test status code
+    assert response.status_code == 200
+
+    # Try to assign for non-existing newspaper
+    response = client.post(f'/editor/{editor_id}/newspapers',
+                           json={"paper_id": 101010})
+    assert response.status_code == 404
+
+    # Try to assign for non-existing editor
+    response = client.post(f'/editor/{1}/newspapers',
+                           json={"paper_id": paper_id})
+    assert response.status_code == 404
+
+
+def test_list_all_editor_issues(client, agency):
+    # Add the editor
+    new_editor = client.post('/editor/',
+                             json={
+                                 "editor_id": 10000,
+                                 "editor_name": "Ana",
+                                 "address": "San Francisco"
+                             })
+    parsed = new_editor.get_json()
+    editor_response = parsed["editor"]
+    editor_id = editor_response["editor_id"]
+
+    response = client.get(f'/editor/{editor_id}/issues')
+    list_of_issues = response.get_json()
+
+    # test status code
+    assert response.status_code == 200
+
+
